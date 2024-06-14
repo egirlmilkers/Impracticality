f8d435b3296c717d6c40b460a51f8fc620efd163
unrvldswtr <37597167+CheezyAmuzus@users.noreply.github.com>
2024-06-14 00:21:04 -0700

    Template base

---
+++ src/client/java/mommiissues/impracticality/ImpracticalityClient.java
@@ -0,0 +1,10 @@
+package mommiissues.impracticality;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ImpracticalityClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
