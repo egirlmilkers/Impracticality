8b31f11959dea49cd5b309894c97fb59e3d9ec3f
unrvldswtr <37597167+CheezyAmuzus@users.noreply.github.com>
2024-06-17 00:03:08 -0700

    Added flesh blocks and ground variants. Added sparklies

---
+++ src/main/java/mommiissues/impracticality/items/Sparklies.java
@@ -0,0 +1,11 @@
+package mommiissues.impracticality.items;
+
+import net.minecraft.item.Item;
+
+public class Sparklies extends Item
+{
+    public Sparklies(Settings settings)
+    {
+        super(settings);
+    }
+}
